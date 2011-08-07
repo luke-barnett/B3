3ed4ffc7f576a8623cfb6ddbb50acd33a618c1e8
Steven McTainsh <smctainsh@gmail.com>
2011-08-08 11:35:30 +1200

    Created tests for Decimal Degrees GPS coordinate getters and setters.
    Signed-off-by: Steven McTainsh <smctainsh@gmail.com>

---
+++ IndiaTangoProject/IndiaTangoProject/Models/GPSCoords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IndiaTangoProject.Models
+{
+    public class GPSCoords
+    {
+        private float _latitude;
+        private float _longitude;
+
+        public GPSCoords(float latitude, float longitude)
+        {
+            _latitude = latitude;
+            _longitude = longitude;
+        }
+
+        public float DecimalDegreesLatitude { get { return _latitude; } set { _latitude = value; } }
+
+        public float DecimalDegreesLongitude { get { return _longitude; } set { _longitude = value; } }
+    }
+}
---
+++ IndiaTangoProject/IndiaTangoTests/GPSCoordsTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using IndiaTangoProject.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IndiaTangoTests
+{
+    [TestClass]
+    public class GPSCoordsTest
+    {
+        [TestMethod]
+        public void GetGPSCoordsDecimalDegrees()
+        {
+            GPSCoords degreeTest = new GPSCoords(-37.788047f, 175.310512f);
+
+            Assert.AreEqual(-37.788047f, degreeTest.DecimalDegreesLatitude);
+            Assert.AreEqual(175.310512f, degreeTest.DecimalDegreesLongitude);
+        }
+
+        [TestMethod]
+        public void SetGPSCoordsDecimalDegrees()
+        {
+            GPSCoords degreeTest = new GPSCoords(0, 0);
+            degreeTest.DecimalDegreesLatitude = -37.788047f;
+
+            Assert.AreEqual(-37.788047f, degreeTest.DecimalDegreesLatitude);
+
+            degreeTest.DecimalDegreesLongitude = 175.310512f;
+
+            Assert.AreEqual(175.310512f, degreeTest.DecimalDegreesLongitude);
+        }
+    }
+}
