3719af2f9f31e64e1488a8488a1135d03786bbf9
Steven McTainsh <smctainsh@gmail.com>
2011-08-08 12:11:48 +1200

    Added further tests for DMS GPS coordinates.
    Signed-off-by: Steven McTainsh <smctainsh@gmail.com>

--- IndiaTangoProject/IndiaTangoProject/Models/GPSCoords.cs
+++ IndiaTangoProject/IndiaTangoProject/Models/GPSCoords.cs
@@ -4,17 +4,42 @@ namespace IndiaTangoProject.Models
 {
     public class GPSCoords
     {
-        private float _latitude;
-        private float _longitude;
+        private decimal _latitude;
+        private decimal _longitude;
 
-        public GPSCoords(float latitude, float longitude)
+        public GPSCoords(decimal latitude, decimal longitude)
         {
             _latitude = latitude;
             _longitude = longitude;
         }
 
-        public float DecimalDegreesLatitude { get { return _latitude; } set { _latitude = value; } }
+        public GPSCoords(string latitude, string longitude)
+        {
+            _latitude = ConvertDMSToDecimalDegrees(latitude);
+            _longitude = ConvertDMSToDecimalDegrees(longitude);
+        }
 
-        public float DecimalDegreesLongitude { get { return _longitude; } set { _longitude = value; } }
+        private decimal ConvertDMSToDecimalDegrees(string coordinate)
+        {
+            decimal degrees;
+            decimal minutes;
+            decimal seconds;
+
+            string[] components = coordinate.Split(' ');
+
+            degrees = decimal.Parse(components[0].Substring(1));
+
+            minutes = decimal.Parse(components[1]) / 60;
+
+            seconds = decimal.Parse(components[2]) / (60 * 60);
+
+            return coordinate.StartsWith("S") || coordinate.StartsWith("W") ? (degrees + minutes + seconds) * -1 : degrees + minutes + seconds;
+        }
+
+        public decimal DecimalDegreesLatitude { get { return _latitude; } set { _latitude = value; } }
+
+        public decimal DecimalDegreesLongitude { get { return _longitude; } set { _longitude = value; } }
+
+        public string DMSLatitude { get { return "0";  } }
     }
 }
--- IndiaTangoProject/IndiaTangoTests/GPSCoordsTest.cs
+++ IndiaTangoProject/IndiaTangoTests/GPSCoordsTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using IndiaTangoProject.Models;
@@ -9,26 +10,36 @@ namespace IndiaTangoTests
     [TestClass]
     public class GPSCoordsTest
     {
+        private const double PRECISION = 0.001;
+
         [TestMethod]
         public void GetGPSCoordsDecimalDegrees()
         {
-            GPSCoords degreeTest = new GPSCoords(-37.788047f, 175.310512f);
+            GPSCoords degreeTest = new GPSCoords(-37.788047M, 175.310512M);
 
-            Assert.AreEqual(-37.788047f, degreeTest.DecimalDegreesLatitude);
-            Assert.AreEqual(175.310512f, degreeTest.DecimalDegreesLongitude);
+            Assert.AreEqual(-37.788047M, degreeTest.DecimalDegreesLatitude);
+            Assert.AreEqual(175.310512M, degreeTest.DecimalDegreesLongitude);
         }
 
         [TestMethod]
         public void SetGPSCoordsDecimalDegrees()
         {
             GPSCoords degreeTest = new GPSCoords(0, 0);
-            degreeTest.DecimalDegreesLatitude = -37.788047f;
+            degreeTest.DecimalDegreesLatitude = -37.788047M;
 
-            Assert.AreEqual(-37.788047f, degreeTest.DecimalDegreesLatitude);
+            Assert.AreEqual(-37.788047M, degreeTest.DecimalDegreesLatitude);
 
-            degreeTest.DecimalDegreesLongitude = 175.310512f;
+            degreeTest.DecimalDegreesLongitude = 175.310512M;
 
-            Assert.AreEqual(175.310512f, degreeTest.DecimalDegreesLongitude);
+            Assert.AreEqual(175.310512M, degreeTest.DecimalDegreesLongitude);
+        }
+
+        [TestMethod]
+        public void GetGPSCoordinatesDMS()
+        {
+            GPSCoords dmsTest = new GPSCoords("S37 47 16", "E175 18 37");
+
+            Assert.AreEqual(-37.788047d, Convert.ToDouble(dmsTest.DecimalDegreesLatitude), PRECISION);
         }
     }
 }
