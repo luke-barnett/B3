31cb965f77ce6226d118d7c57140ad1381b0a397
Luke Barnett <l@en.vc>
2011-08-09 09:36:26 +1200

    Reset the project.
    Now uses the NUnitFramework :D
    
    Signed-off-by: Luke Barnett <l@en.vc>

---
+++ IndiaTango/IndiaTango.Tests/GPSCoordsTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using IndiaTango.Models;
+using NUnit.Framework;
+
+
+namespace IndiaTango.Tests
+{
+    [TestFixture]
+    public class GPSCoordsTest
+    {
+        private const double PRECISION = 0.001;
+
+        [Test]
+        public void GetGPSCoordsDecimalDegrees()
+        {
+            GPSCoords degreeTest = new GPSCoords(-37.788047M, 175.310512M);
+
+            Assert.AreEqual(-37.788047M, degreeTest.DecimalDegreesLatitude);
+            Assert.AreEqual(175.310512M, degreeTest.DecimalDegreesLongitude);
+        }
+
+        [Test]
+        public void SetGPSCoordsDecimalDegrees()
+        {
+            GPSCoords degreeTest = new GPSCoords(0, 0);
+            degreeTest.DecimalDegreesLatitude = -37.788047M;
+
+            Assert.AreEqual(-37.788047M, degreeTest.DecimalDegreesLatitude);
+
+            degreeTest.DecimalDegreesLongitude = 175.310512M;
+
+            Assert.AreEqual(175.310512M, degreeTest.DecimalDegreesLongitude);
+        }
+
+        [Test]
+        public void GPSCoordinatesDMSToDecimalDegrees()
+        {
+            GPSCoords dmsTest = new GPSCoords("S37 47 16", "E175 18 37");
+
+            Assert.AreEqual(-37.788047d, Convert.ToDouble(dmsTest.DecimalDegreesLatitude), PRECISION);
+
+            Assert.AreEqual(175.310512d, Convert.ToDouble(dmsTest.DecimalDegreesLongitude), PRECISION);
+        }
+
+        [Test]
+        public void GetGPSCoordinatesDMS()
+        {
+            GPSCoords dmsTest = new GPSCoords("S37 47 16", "E175 18 37");
+
+            Assert.AreEqual("S37 47 15", dmsTest.DMSLatitude); //Pression difference
+
+            Assert.AreEqual("E175 18 37", dmsTest.DMSLongitude);
+        }
+
+        [Test]
+        public void SetGPSCoordinatesDMS()
+        {
+            GPSCoords dmsTest = new GPSCoords("", "");
+            dmsTest.DMSLatitude = "S37 47 16";
+
+            Assert.AreEqual("S37 47 15", dmsTest.DMSLatitude); //Pression difference
+
+            dmsTest.DMSLongitude = "E175 18 37";
+
+            Assert.AreEqual("E175 18 37", dmsTest.DMSLongitude);
+        }
+
+        [Test]
+        public void GPSCoordinatesDecimalDegreesToDMS()
+        {
+            GPSCoords degreeTest = new GPSCoords(-37.788047M, 175.310512M);
+
+            Assert.AreEqual("S37 47 16", degreeTest.DMSLatitude);
+
+            Assert.AreEqual("E175 18 37", degreeTest.DMSLongitude);
+        }
+    }
+}
---
+++ IndiaTango/IndiaTango/Models/GPSCoords.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace IndiaTango.Models
+{
+    public class GPSCoords
+    {
+        private decimal _latitude;
+        private decimal _longitude;
+
+        public GPSCoords(decimal latitude, decimal longitude)
+        {
+            _latitude = latitude;
+            _longitude = longitude;
+        }
+
+        public GPSCoords(string latitude, string longitude)
+        {
+            _latitude = ConvertDMSToDecimalDegrees(latitude);
+            _longitude = ConvertDMSToDecimalDegrees(longitude);
+        }
+
+        private decimal ConvertDMSToDecimalDegrees(string coordinate)
+        {
+            decimal degrees;
+            decimal minutes;
+            decimal seconds;
+
+            string[] components = coordinate.Split(' ');
+
+            if (components.Length == 3)
+            {
+                degrees = decimal.Parse(components[0].Substring(1));
+
+                minutes = decimal.Parse(components[1]) / 60;
+
+                seconds = decimal.Parse(components[2]) / (60 * 60);
+
+                return coordinate.StartsWith("S") || coordinate.StartsWith("W")
+                           ? (degrees + minutes + seconds) * -1
+                           : degrees + minutes + seconds;
+            }
+            else
+                return 0;
+        }
+
+        private string ConvertDecimalDegreesToDMS(decimal coordinate, bool latitude)
+        {
+            decimal degrees;
+            decimal minutes;
+            decimal seconds;
+            string direction = "N";
+
+            if (latitude)
+                direction = (coordinate >= 0) ? "N" : "S";
+            else
+                direction = (coordinate >= 0) ? "E" : "W";
+
+            coordinate = Math.Abs(coordinate);
+
+            degrees = Decimal.Truncate(coordinate);
+            minutes = Decimal.Truncate((coordinate - degrees) * 60);
+            seconds = Decimal.Truncate((((coordinate - degrees) * 60) - minutes) * 60);
+
+            return String.Format("{0}{1} {2} {3}", direction, degrees.ToString(), minutes.ToString(), seconds.ToString());
+        }
+
+        public decimal DecimalDegreesLatitude { get { return _latitude; } set { _latitude = value; } }
+
+        public decimal DecimalDegreesLongitude { get { return _longitude; } set { _longitude = value; } }
+
+        public string DMSLatitude { get { return ConvertDecimalDegreesToDMS(_latitude, true); } set { _latitude = ConvertDMSToDecimalDegrees(value); } }
+
+        public string DMSLongitude { get { return ConvertDecimalDegreesToDMS(_longitude, false); } set { _longitude = ConvertDMSToDecimalDegrees(value); } }
+    }
+}
