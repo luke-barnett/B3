b08b34bf9fcb756946f20ae1856993e3c51bd823
Luke Barnett <l@en.vc>
2011-08-09 14:01:52 +1200

    Added comments to DataValue.cs
    Signed-off-by: Luke Barnett <l@en.vc>

--- IndiaTango/IndiaTango/Models/DataValue.cs
+++ IndiaTango/IndiaTango/Models/DataValue.cs
@@ -1,21 +1,28 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace IndiaTango.Models
 {
     public class DataValue
     {
-        public DateTime Timestamp;
-        public float Value;
+        public DateTime Timestamp { get; set; }
+        public float Value { get; set; }
 
+        /// <summary>
+        /// Creates a new DataValue class with the given time stamp and value
+        /// </summary>
+        /// <param name="timeStamp">The time stamp to use</param>
+        /// <param name="value">The value to use</param>
         public DataValue(DateTime timeStamp, float value)
         {
             Timestamp = timeStamp;
             Value = value;
         }
 
+        /// <summary>
+        /// Checks Equality between this object and any given other
+        /// </summary>
+        /// <param name="obj">The object to check it against</param>
+        /// <returns>The boolean result of the test</returns>
         public override bool Equals(object obj)
         {
             return (obj is DataValue) && (obj as DataValue).Timestamp == Timestamp && (obj as DataValue).Value == Value;
