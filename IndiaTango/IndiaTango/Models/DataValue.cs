ab49d8dba743b50357b9ed6d0da00e7924381403
Luke Barnett <l@en.vc>
2011-08-09 12:40:02 +1200

    Created and Tested DataValue.cs
    Signed-off-by: Luke Barnett <l@en.vc>

---
+++ IndiaTango/IndiaTango/Models/DataValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IndiaTango.Models
+{
+    public class DataValue
+    {
+        public DataValue(float value, DateTime timeStamp)
+        {
+            Value = value;
+            TimeStamp = timeStamp;
+        }
+
+        public float Value { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
