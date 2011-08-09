4180482807f4f1642767b090938e08c80a9883ab
Steven McTainsh <smctainsh@gmail.com>
2011-08-09 13:42:37 +1200

    Added SensorState class and associated tests.
    Signed-off-by: Steven McTainsh <smctainsh@gmail.com>

---
+++ IndiaTango/IndiaTango/Models/DataValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IndiaTango.Models
+{
+    public class DataValue
+    {
+        public DateTime Timestamp;
+        public float Value;
+
+        public DataValue(DateTime timeStamp, float value)
+        {
+            Timestamp = timeStamp;
+            Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj is DataValue) && (obj as DataValue).Timestamp == Timestamp && (obj as DataValue).Value == Value;
+        }
+    }
+}
---
+++ IndiaTango/IndiaTango/Models/SensorState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IndiaTango.Models
+{
+    public class SensorState
+    {
+        private DateTime _editTimestamp;
+        private List<DataValue> _valueList;
+
+        public SensorState(DateTime editTimestamp)
+        {
+            _editTimestamp = editTimestamp;
+        }
+
+        public SensorState(DateTime editTimestamp, List<DataValue> valueList)
+        {
+            _editTimestamp = editTimestamp;
+            _valueList = valueList;
+        }
+
+        public DateTime EditTimestamp { get { return _editTimestamp; } set { _editTimestamp = value; } }
+
+        public List<DataValue> Values { get { return _valueList; } set { _valueList = value; } }
+    }
+}
