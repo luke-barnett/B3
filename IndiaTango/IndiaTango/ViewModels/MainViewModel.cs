d4cc50b5e8c0145463addfe44c6c9a7657f96b5e
Luke Barnett <l@en.vc>
2011-08-15 12:49:40 +1200

    Added the MainView
    Signed-off-by: Luke Barnett <l@en.vc>

--- IndiaTango/IndiaTango/ViewModels/MainViewModel.cs
+++ IndiaTango/IndiaTango/ViewModels/MainViewModel.cs
@@ -1,9 +1,21 @@
 ﻿
+using System.Reflection;
+using System.Windows;
+
 namespace IndiaTango.ViewModels
 {
     public class MainViewModel
     {
 
         public string Title { get { return "INDIA TANGO"; } }
+        public string Version { get { return string.Format("[alpha version {0}]", Assembly.GetExecutingAssembly().GetName().Version.ToString()); }}
+        public string Creators { get { return "Developed by:\r\nSteven McTanish\r\nLuke Barnett\r\nMichael Baumberger\r\nKerry Arts"; } }
+
+        
+
+        public void BtnLoad()
+        {
+            
+        }
     }
 }
