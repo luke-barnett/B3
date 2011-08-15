b2778fe84d8757448490a53caff8bbe644a6c0b0
Luke Barnett <l@en.vc>
2011-08-15 14:38:56 +1200

    Wrote CSVReader.cs and some tests for it
    Signed-off-by: Luke Barnett <l@en.vc>

--- IndiaTango/IndiaTango.Tests/CsvReaderTest.cs
+++ IndiaTango/IndiaTango.Tests/CsvReaderTest.cs
@@ -1,26 +1,26 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
+using System;
 using NUnit.Framework;
 using IndiaTango.Models;
 
 namespace IndiaTango.Tests
 {
     [TestFixture]
-    class CsvReaderTest
+    class CSVReaderTest
     {
-        private CsvReader _reader;
+        private CSVReader _reader;
+        private const string TestDataFileName = "../../Test Data/lakeTutira120120110648.csv";
 
         [SetUp]
         public void Setup()
         {
-            _reader = new CsvReader("../../../../../lakeTutira120120110648.csv");
+            _reader = new CSVReader(TestDataFileName);
         }
 
         [Test]
         public void ConstructorTest()
         {
-            var reader = new CsvReader("../../../../../lakeTutira120120110648.csv");
+            var reader = new CSVReader(TestDataFileName);
         }
 
         [Test]
@@ -28,7 +28,35 @@ namespace IndiaTango.Tests
         {
             var sensors = _reader.ReadSensors();
             Assert.NotNull(sensors);
+            Assert.IsTrue(sensors.Count > 0);
+            Assert.AreEqual(27, sensors.Count);
+            Assert.IsTrue(sensors[0].CurrentState.Values.Count != 0);
+
+            var sensorNames = (new string[] { "Temperature1", "Temperature2", "Temperature3", "Temperature4", "Temperature5", "Temperature6", "Temperature7", "Temperature8", "Temperature9", "Temperature10", "BatteryVolts", "DOSurfaceSat", "DODeepSat", "Chlorophyll", "Phycocyanin", "Turbidity", "LightLevel", "DOSurfaceCont", "DODeepCont", "WaterColumnDepth(ABS)", "WindDirection", "WindSpeed", "AirTemperature", "Humidity", "BarometricPressure", "Rainfall", "Hail" }).ToList();
+
+            foreach(var sensor in sensors)
+            {
+                Assert.IsTrue(sensorNames.Contains(sensor.Name));
+                sensorNames.Remove(sensor.Name);
+            }
+
+            var hailSensor = sensors.Where(sensor => sensor.Name == "Hail").First();
+            Assert.IsNotNull(hailSensor);
+            Assert.AreEqual(53873, hailSensor.CurrentState.Values.Count);
         }
 
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FileNotFound()
+        {
+            _reader = new CSVReader("NotEvenAFile.csv");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void BadFileName()
+        {
+            _reader = new CSVReader("CannotTellIfIAmCSV");
+        }
     }
 }
--- IndiaTango/IndiaTango/Models/CsvReader.cs
+++ IndiaTango/IndiaTango/Models/CsvReader.cs
@@ -1,54 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace IndiaTango.Models
 {
-    public class CsvReader : IDataReader
+    public class CSVReader : IDataReader
     {
-        private List<Sensor> _sensors = new List<Sensor>();
+        private string _filename;
+        private Sensor[] sensors;
 
-        public CsvReader(string fileName)
+        public CSVReader(string fileName)
         {
-            try
-            {
-                var reader = new StreamReader(fileName);
-                var line = reader.ReadLine();
-                var parts = line.Split(',');
-                
-                //first element is date, second is time
-                for (var i = 2; i < parts.Length; i++)
-                {
-                    _sensors.Add(new Sensor(parts[i], "C"));
-                }
+            if (!fileName.EndsWith(".csv"))
+                throw new ArgumentException("Can't tell if this is a csv (filetype not .csv)");
 
-                while (reader.Peek() != -1)
-                {
-                    line = reader.ReadLine();
-                    parts = line.Split(',');
-                    var date = parts[0].Split('/');
-                    var time = parts[1].Split(':');
-                    var timeStamp = new DateTime(Int32.Parse(date[2]), Int32.Parse(date[1]), Int32.Parse(date[0]),Int32.Parse(time[0]),Int32.Parse(time[1]),0);
-                    var sState = new SensorState(timeStamp);
-                    //first element is date, second is time
-                    for (var i = 2; i < parts.Length; i++)
-                    {
-                        //_sensors[i]
-                    }
-                }
+            if (!File.Exists(fileName))
+                throw new ArgumentException("File wasn't found");
 
-
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
-
+            _filename = fileName;
         }
 
         public List<Sensor> ReadSensors()
         {
-            return null;
+            if (sensors != null)
+                return sensors.ToList();
+
+            sensors = new Sensor[0];
+            using (var sr = new StreamReader(_filename))
+            {
+                var sensorNamesString = sr.ReadLine();
+
+                String[] sensorNames;
+                if(sensorNamesString != null)
+                    sensorNames = sensorNamesString.Split(',');
+                else
+                    throw new FormatException("Couldn't get the sensor names from the csv");
+                
+                //First two are the time stamp
+                sensors = new Sensor[sensorNames.Length - 2];
+
+                for (int i = 2; i < sensorNames.Length; i++ )
+                {
+                    sensors[i - 2] = new Sensor(sensorNames[i], null);
+                    sensors[i -2].AddState(new SensorState(DateTime.Now));
+                }
+
+
+                String readLine = null;
+                while((readLine = sr.ReadLine()) != null)
+                {
+                    var values = readLine.Split(',');
+                    if(values.Length != sensorNames.Length)
+                        throw new FormatException("Number of values mismatch from the number of sensors");
+                    var timeStamp = DateTime.Parse(values[0] + " " + values[1]);
+
+                    for (int i = 2; i < values.Length; i++)
+                    {
+                        try
+                        {
+                            sensors[i - 2].CurrentState.Values.Add(new DataValue(timeStamp, float.Parse(values[i])));
+                        }
+                        catch(Exception e)
+                        {
+                            
+                        }
+                    }
+                }
+
+            }
+            
+            return sensors.ToList();
         }
 
         public object ReadBuoy()
