ba1cc3176532fba42065b367da6384d6b2e75b2a
Luke Barnett <l@en.vc>
2011-08-16 14:05:30 +1200

    Added in a chart framework, currently trying to get binding of the series to work
    Signed-off-by: Luke Barnett <l@en.vc>

--- IndiaTango/IndiaTango/ViewModels/GraphViewModel.cs
+++ IndiaTango/IndiaTango/ViewModels/GraphViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Caliburn.Micro;
+﻿using System;
+using Caliburn.Micro;
+using Visiblox.Charts;
 
 namespace IndiaTango.ViewModels
 {
@@ -12,5 +14,28 @@ namespace IndiaTango.ViewModels
             _windowManager = windowManager;
             _container = container;
         }
+
+        public void InitialiseGraph()
+        {
+            LineSeries l = new LineSeries();
+            DataSeries<DateTime, float> data = new DataSeries<DateTime, float>();
+            Random generator = new Random();
+
+            for (int i = 0; i < 50; i++)
+            {
+                data.Add(DateTime.Now, (float)generator.NextDouble());
+            }
+            l.DataSeries = data;
+            _chartSeries.Add(l);
+            NotifyOfPropertyChange("ChartSeries");
+        }
+
+        private SeriesCollection<IChartSeries> _chartSeries;
+        public SeriesCollection<IChartSeries> ChartSeries { get
+        {
+         if(_chartSeries == null)
+             _chartSeries = new SeriesCollection<IChartSeries>();
+            return _chartSeries;
+        } set { _chartSeries = value; } }
     }
 }
