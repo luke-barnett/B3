cbcdd78178b13452a082a2da47696ffb6ff65ec7
Steven McTainsh <smctainsh@gmail.com>
2011-08-21 23:33:27 +1200

    Added the beginnings of the buoy details view and view model, to allow for input of buoy information.
    Signed-off-by: Steven McTainsh <smctainsh@gmail.com>

--- IndiaTango/IndiaTango/Bootstrapper.cs
+++ IndiaTango/IndiaTango/Bootstrapper.cs
@@ -16,6 +16,7 @@ namespace IndiaTango
             _container.RegisterSingleton(typeof(MainViewModel), "MainViewModel", typeof(MainViewModel));
             _container.RegisterPerRequest(typeof(GraphViewModel), "GraphViewModel", typeof(GraphViewModel));
             _container.RegisterPerRequest(typeof(SessionViewModel), "SessionViewModel", typeof(SessionViewModel));
+            _container.RegisterPerRequest(typeof(BuoyDetailsViewModel), "BuoyDetailsViewModel", typeof(BuoyDetailsViewModel));
 
             _container.RegisterInstance(typeof(IWindowManager), null, new WindowManager());
             _container.RegisterInstance(typeof(SimpleContainer), null, _container);
---
+++ IndiaTango/IndiaTango/ViewModels/BuoyDetailsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+
+namespace IndiaTango.ViewModels
+{
+    class BuoyDetailsViewModel : BaseViewModel
+    {
+        private readonly IWindowManager _windowManager;
+        private readonly SimpleContainer _container;
+
+        public BuoyDetailsViewModel(IWindowManager windowManager, SimpleContainer container)
+        {
+            _windowManager = windowManager;
+            _container = container;
+        }
+
+
+    }
+}
