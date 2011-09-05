f20c3ad305ebeebf22c8211548a33dbfd3d0515b
Luke Barnett <l@en.vc>
2011-09-05 15:20:12 +1200

    Added a GraphableSensor.cs and some relevant tests
    Signed-off-by: Luke Barnett <l@en.vc>

---
+++ IndiaTango/IndiaTango.Tests/GraphableSensorTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Media;
+using IndiaTango.Models;
+using NUnit.Framework;
+
+namespace IndiaTango.Tests
+{
+    [TestFixture]
+    class GraphableSensorTest
+    {
+        private GraphableSensor sensor;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var rawSensor = new Sensor("Temperature", "Temperature at 30m", 40, 20, "C", 20, "Temperature Sensors Ltd.", "1102123");
+
+            rawSensor.AddState(new SensorState(DateTime.Now));
+            rawSensor.CurrentState.Values.Add(new DataValue(DateTime.Now,15));
+            rawSensor.CurrentState.Values.Add(new DataValue(DateTime.Now, 20));
+            rawSensor.CurrentState.Values.Add(new DataValue(DateTime.Now, 25));
+
+            sensor = new GraphableSensor(rawSensor);
+        }
+        
+        [Test]
+        public void ColourTesting()
+        {
+            Assert.IsNotNull(sensor.Colour);
+
+            sensor.Colour = Brushes.Black;
+
+            Assert.AreEqual(sensor.Colour, Brushes.Black);
+        }
+
+        [Test]
+        public void GetSensor()
+        {
+            Assert.IsNotNull(sensor.Sensor);
+        }
+
+        [Test]
+        public void DataPointsTest()
+        {
+            var dataPoints = sensor.DataPoints.ToArray();
+            Assert.AreEqual(dataPoints[0].Y, 15);
+            Assert.AreEqual(dataPoints[1].Y, 20);
+            Assert.AreEqual(dataPoints[2].Y, 25);
+        }
+    }
+}
---
+++ IndiaTango/IndiaTango/Models/GraphableSensor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media;
+using Visiblox.Charts;
+
+namespace IndiaTango.Models
+{
+    /// <summary>
+    /// Wraps a sensor with some needed graph data
+    /// </summary>
+    public class GraphableSensor
+    {
+        /// <summary>
+        /// Creates a new GraphableSensor based on the given sensor
+        /// </summary>
+        /// <param name="baseSensor">The sensor to base it on</param>
+        public GraphableSensor(Sensor baseSensor)
+        {
+            Sensor = baseSensor;
+            //Create a random colour
+            var generator = new Random();
+            Colour = new SolidColorBrush(Color.FromRgb((byte)generator.Next(255), (byte)generator.Next(255), (byte)generator.Next(255)));
+
+            DataPoints = from dataValue in Sensor.CurrentState.Values select new DataPoint<DateTime, float>(dataValue.Timestamp, dataValue.Value);
+        }
+
+        /// <summary>
+        /// The colour to use for the line series
+        /// </summary>
+        public Brush Colour { get; set; }
+
+        /// <summary>
+        /// The sensor to base it all on
+        /// </summary>
+        public Sensor Sensor { get; private set; }
+
+        /// <summary>
+        /// The datapoints to use
+        /// </summary>
+        public IEnumerable<DataPoint<DateTime, float>> DataPoints { get; private set; }
+
+        /// <summary>
+        /// Re extracts the data points from the sensor
+        /// </summary>
+        public void RefreshDataPoints()
+        {
+            DataPoints = from dataValue in Sensor.CurrentState.Values select new DataPoint<DateTime, float>(dataValue.Timestamp, dataValue.Value);
+        }
+    }
+}
