79f948e52c7d096ca3fce899d5a35a35baae9717
Michael Baumberger <baumbydaswiss@gmail.com>
2011-09-06 13:54:40 +1200

    Extrapolation is faster, still not fast enough though
    Signed-off-by: Michael Baumberger <baumbydaswiss@gmail.com>

--- IndiaTango/IndiaTango/ViewModels/MissingValuesViewModel.cs
+++ IndiaTango/IndiaTango/ViewModels/MissingValuesViewModel.cs
@@ -16,7 +16,7 @@ namespace IndiaTango.ViewModels
         private Sensor _sensor;
     	private int _zoomLevel = 100;
 		private List<DateTime> _missingValues = new List<DateTime>();
-		private List<DataValue> _selectedValues = new List<DataValue>();
+		private List<DateTime> _selectedValues = new List<DateTime>();
     	//private List<Sensor> _sensorList = new List<Sensor>();
         private Dataset _ds;
 
@@ -96,7 +96,7 @@ namespace IndiaTango.ViewModels
 			}
 		}
 
-    	public List<DataValue> SelectedValues
+    	public List<DateTime> SelectedValues
     	{
     		get { return _selectedValues; } 
 			set
@@ -112,12 +112,12 @@ namespace IndiaTango.ViewModels
 
 		public void SelectionChanged(SelectionChangedEventArgs e)
 		{
-			foreach (DataValue item in e.RemovedItems)
+			foreach (DateTime item in e.RemovedItems)
 			{
 				SelectedValues.Remove(item);
 			}
 
-			foreach (DataValue item in e.AddedItems)
+			foreach (DateTime item in e.AddedItems)
 			{
 				SelectedValues.Add(item);
 			}
@@ -147,26 +147,30 @@ namespace IndiaTango.ViewModels
             if(_selectedValues.Count == 0)
                 return;
 
-			foreach (DataValue dataValue in SelectedValues)
+			foreach (DateTime time in SelectedValues)
         	{
-				DataValue prevValue = null;
-				var time = 15;
-
-				while (prevValue == null)
-				{
-					prevValue = _sensor.CurrentState.Values.Find(dv => dv.Timestamp.AddMinutes(time) == dataValue.Timestamp);
-					time += 15;
-				}
-				var newDV = new DataValue(dataValue.Timestamp, 0);
+				var prevValue = FindPrevValue(time);
+        	    var newDV = new DataValue(time, 0);
 				_sensor.CurrentState.Values.Insert(_sensor.CurrentState.Values.FindIndex(delegate(DataValue dv)
 				{ return dv == prevValue; }) + 1, newDV);
         	}
             
-            MissingValues = _sensor.CurrentState.GetMissingTimes(15,_ds.StartTimeStamp,_ds.EndTimeStamp);
-            NotifyOfPropertyChange(()=>MissingValues);
-			NotifyOfPropertyChange(() => SelectedSensor);
+            Cleanup();
 
 			Common.ShowMessageBox("Values Updated", "The selected values have been set to 0.", false, false);
+        }
+
+        private DataValue FindPrevValue(DateTime dataValue)
+        {
+            DataValue prevValue = null;
+            var time = 15;
+
+            while (prevValue == null)
+            {
+                prevValue = _sensor.CurrentState.Values.Find(dv => dv.Timestamp.AddMinutes(time) == dataValue);
+                time += 15;
+            }
+            return prevValue;
         }
 
         public void btnSpecify()
@@ -196,31 +200,28 @@ namespace IndiaTango.ViewModels
                 }
             }
 
-			foreach (var dataValue in SelectedValues)
+			foreach (var time in SelectedValues)
 			{
-				DataValue prevValue = null;
-				var time = 15;
+                var prevValue = FindPrevValue(time);
 
-				while (prevValue == null)
-				{
-					prevValue = _sensor.CurrentState.Values.Find(dv => dv.Timestamp.AddMinutes(time) == dataValue.Timestamp);
-					time += 15;
-				}
-
-				var newDV = new DataValue(dataValue.Timestamp, value);
+				var newDV = new DataValue(time, value);
 				_sensor.CurrentState.Values.Insert(_sensor.CurrentState.Values.FindIndex(delegate(DataValue dv)
 				                                                                         	{ return dv == prevValue; }) + 1, newDV);
 			}
 
-        	_missingValues = _sensor.CurrentState.GetMissingTimes(15,_ds.StartTimeStamp,_ds.EndTimeStamp);
-            NotifyOfPropertyChange(() => MissingValues);
-			NotifyOfPropertyChange(() => SelectedSensor);
-            NotifyOfPropertyChange(() => MissingCount);
+        	Cleanup();
 
-        	Common.ShowMessageBox("Values Updated", "The selected values have been set to " + value + ".", false, false);
+            Common.ShowMessageBox("Values Updated", "The selected values have been set to " + value + ".", false, false);
         }
 
-		public void btnExtrapolate()
+        private void Cleanup()
+        {
+            _missingValues = _sensor.CurrentState.GetMissingTimes(15, _ds.StartTimeStamp, _ds.EndTimeStamp);
+            NotifyOfPropertyChange(() => MissingValues);
+            NotifyOfPropertyChange(() => MissingCount);
+        }
+
+        public void btnExtrapolate()
 		{
 			//TODO: Refactor
             if (SelectedValues.Count == 0)
@@ -230,40 +231,30 @@ namespace IndiaTango.ViewModels
                 return;
             }
 		    var first = SelectedValues[0];
-		    DataValue startValue = null;
+            var startValue = FindPrevValue(first);
 		    DataValue endValue = null;
 		    var time = -15;
             while (endValue == null)
             {
-                endValue = _sensor.CurrentState.Values.Find(dv => dv.Timestamp.AddMinutes(time) == first.Timestamp);
+                endValue = _sensor.CurrentState.Values.Find(dv => dv.Timestamp.AddMinutes(time) == first);
                 time -= 15;
             }
-		    time = 15;
-            while (startValue == null)
-            {
-                startValue = _sensor.CurrentState.Values.Find(dv => dv.Timestamp.AddMinutes(time) == first.Timestamp);
-                time += 15;
-            }
+            var tempList = new List<DataValue>();
 		    var timeDiff = endValue.Timestamp.Subtract(startValue.Timestamp).TotalMinutes;
 		    var valDiff = endValue.Value - startValue.Value;
 		    var step = valDiff/(timeDiff/15);
 		    var value = startValue.Value + step;
             var newDV = new DataValue(startValue.Timestamp.AddMinutes(15), (float)value);
-            _sensor.CurrentState.Values.Insert(_sensor.CurrentState.Values.FindIndex(delegate(DataValue dv)
-                { return dv == startValue; }) + 1, newDV);
-		    var prevDV = newDV;
+            tempList.Add(newDV);
             for(var i = 30;i<timeDiff;i+=15)
             {
                 newDV = new DataValue(startValue.Timestamp.AddMinutes(i), (float)value);
-                _sensor.CurrentState.Values.Insert(_sensor.CurrentState.Values.FindIndex(delegate(DataValue dv)
-                { return dv == prevDV; }) + 1, newDV);
-                prevDV = newDV;
+                tempList.Add(newDV);
                 value += step;
             }
-
-            _missingValues = _sensor.CurrentState.GetMissingTimes(15,_ds.StartTimeStamp,_ds.EndTimeStamp);
-            NotifyOfPropertyChange(() => MissingValues);
-            NotifyOfPropertyChange(() => MissingCount);
+            _sensor.CurrentState.Values.InsertRange(_sensor.CurrentState.Values.FindIndex(delegate(DataValue dv)
+                { return dv == startValue; })+1,tempList);
+            Cleanup();
 
             Common.ShowMessageBox("Values Updated", "The vaues have been extrapolated", false, false);
 		}
