e74b071cd87a4aa2f7161f66e7ca8e61e4285f40
Kerry Arts <karts41@gmail.com>
2011-09-12 15:18:52 +1200

    Finished GUI for export options. Also changed parameters for Export() in DatasetExporter.cs
    Signed-off-by: Kerry Arts <karts41@gmail.com>

--- IndiaTango/IndiaTango/ViewModels/ExportViewModel.cs
+++ IndiaTango/IndiaTango/ViewModels/ExportViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -13,16 +14,69 @@ namespace IndiaTango.ViewModels
         private IWindowManager _windowManager;
         private SimpleContainer _container;
         private Dataset _dataset;
-
-        public string Title
-        {
-            get { return "Export Options"; }
-        }
+        private bool _includeEmptyLines = true;
+        private bool _includeMetaData = false;
+        private bool _includeChangeLog = false;
+        private DateColumnFormat _dateColumnFormat;
+        private ExportedPoints _exportedPoints;
 
         public ExportViewModel(IWindowManager windowManager, SimpleContainer container)
         {
             _windowManager = windowManager;
             _container = container;
+
+            _dateColumnFormat = DateColumnFormat.TwoDateColumn;
+            _exportedPoints = ExportedPoints.AllPoints;
+        }
+
+        #region View Properties
+        public string Title
+        {
+            get { return "Export Options"; }
+        }
+
+        public List<DateColumnFormat> DateColumnFormatOptions
+        {
+            get { return new List<DateColumnFormat>(new[] { DateColumnFormat.TwoDateColumn, DateColumnFormat.SplitDateColumn }); }
+        }
+
+        public List<ExportedPoints> ExportedPointsOptions
+        {
+            get
+            {
+                return new List<ExportedPoints>(new[] { ExportedPoints.AllPoints,ExportedPoints.HourlyPoints,
+                    ExportedPoints.DailyPoints,ExportedPoints.WeeklyPoints }); 
+            }
+        }
+
+        public DateColumnFormat DateColumnFormat
+        {
+            get { return _dateColumnFormat; }
+            set { _dateColumnFormat = value; NotifyOfPropertyChange(() => DateColumnFormat); Debug.WriteLine("column format changed: " + value.Name); }
+        }
+
+        public ExportedPoints ExportedPoints
+        {
+            get { return _exportedPoints; }
+            set { _exportedPoints = value; NotifyOfPropertyChange(() => ExportedPoints); Debug.WriteLine("export format changed: " + value.Name); }
+        }
+
+        public bool IncludeEmptyLines
+        {
+            get { return _includeEmptyLines; }
+            set { _includeEmptyLines = value; NotifyOfPropertyChange(() => IncludeEmptyLines); }
+        }
+
+        public bool IncludeMetaData
+        {
+            get { return _includeMetaData; }
+            set { _includeMetaData = value; NotifyOfPropertyChange(() => IncludeMetaData);}
+        }
+
+        public bool IncludeChangeLog
+        {
+            get { return _includeChangeLog; }
+            set { _includeChangeLog = value; NotifyOfPropertyChange(() => IncludeChangeLog);}
         }
 
         public Dataset Dataset
@@ -31,7 +85,11 @@ namespace IndiaTango.ViewModels
             set { _dataset = value; }
         }
 
-        public void btnSave()
+
+        #endregion
+
+        #region Event Handlers
+        public void btnExport()
         {
             var dialog = new SaveFileDialog();
             dialog.Filter = ExportFormat.CSV.FilterText;
@@ -39,7 +97,8 @@ namespace IndiaTango.ViewModels
             if (dialog.ShowDialog() == DialogResult.OK)
             {
                 DatasetExporter exporter = new DatasetExporter(Dataset);
-                exporter.Export(dialog.FileName, ExportFormat.CSV, true, dialog.FilterIndex == 2);
+                exporter.Export(dialog.FileName, ExportFormat.CSV, IncludeEmptyLines, IncludeMetaData, IncludeChangeLog, ExportedPoints, DateColumnFormat);
+                this.TryClose();
             }
         }
 
@@ -47,6 +106,9 @@ namespace IndiaTango.ViewModels
         {
             this.TryClose();
         }
-
+    
+        #endregion
     }
+
+    
 }
