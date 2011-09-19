0deeff93fad77a48f041905f55b6deb76154fdfc
Kerry Arts <karts41@gmail.com>
2011-09-19 12:07:39 +1200

    Woot copy-pasta'd a formula evaluator. Just inital tests and basic functionality as of now
    Signed-off-by: Kerry Arts <karts41@gmail.com>

---
+++ IndiaTango/IndiaTango.Tests/FormulaEvaluatorTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using NUnit.Framework;
+using IndiaTango.Models;
+
+namespace IndiaTango.Tests
+{
+	[TestFixture]
+	class FormulaEvaluatorTest
+	{
+	    public FormulaEvaluator eval;
+
+		[SetUp]
+		public void Setup()
+		{
+		    eval = new FormulaEvaluator();
+		}
+
+		[Test]
+		public void OnePlusOneTest()
+		{
+            Assert.AreEqual(2,eval.EvaluateFormula("1+1"));
+		}
+
+        [Test]
+        public void ForLoopTest()
+        {
+            Assert.AreEqual(10, eval.EvaluateFormula("answer;\ndouble total = 0;\nfor(int i = 0; i < 5; i++)\ntotal+=i;\nanswer=total;"));
+        }
+	}
+}
