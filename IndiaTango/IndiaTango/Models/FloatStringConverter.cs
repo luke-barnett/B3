60eb8d5f5e4ee291eac2c7dac087d3936a55d0fe
Luke Barnett <luke@barnett.net.nz>
2011-11-18 10:57:56 +1300

    Missed the FloatStringConverter.cs file
    Signed-off-by: Luke Barnett <luke@barnett.net.nz>

---
+++ IndiaTango/IndiaTango/Models/FloatStringConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace IndiaTango.Models
+{
+    [ValueConversion(typeof(float), typeof(string))]
+    public class FloatStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? value.ToString() : "0";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return value != null ? float.Parse((string)value) : 0;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
