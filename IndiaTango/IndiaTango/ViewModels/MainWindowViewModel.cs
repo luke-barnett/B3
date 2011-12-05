c8806f922ed2ca1cca68ce7752bd8758d057462d
Luke Barnett <luke@barnett.net.nz>
2011-12-05 16:51:06 +1300

    It would probably pay to check on enable
    Signed-off-by: Luke Barnett <luke@barnett.net.nz>

--- IndiaTango/IndiaTango/ViewModels/MainWindowViewModel.cs
+++ IndiaTango/IndiaTango/ViewModels/MainWindowViewModel.cs
@@ -2273,6 +2273,7 @@ namespace IndiaTango.ViewModels
         public void EnableDetectionMethods()
         {
             _detectionMethodsEnabled = true;
+            CheckTheseMethods(_detectionMethods.Where(x => x.IsEnabled));
         }
 
         public void DisableDetectionMethods()
