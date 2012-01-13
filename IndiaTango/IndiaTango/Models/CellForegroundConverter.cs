350ab99c70b04fdc006ea964923183f3008ddf99
Luke Barnett <luke@barnett.net.nz>
2012-01-13 13:25:50 +1300

    When there is an non raw value for a sensor at a timestamp the row is coloured red

---
+++ IndiaTango/IndiaTango/Models/CellForegroundConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace IndiaTango.Models
+{
+    [ValueConversion(typeof(string), typeof(Brush))]
+    public class CellForegroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is DataRowView))
+                return Brushes.Black;
+
+            var rowView = (DataRowView) value;
+
+            foreach (var item in rowView.Row.ItemArray.Where(item => item is string).Cast<string>())
+            {
+                if (item == "")
+                    return Brushes.Black;
+
+                if (item.StartsWith("["))
+                    return Brushes.Red;
+
+                var parts = item.Split(' ');
+                if (parts[0] != parts[1].Replace("[", "").Replace("]", ""))
+                    return Brushes.Red;
+            }
+            
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
