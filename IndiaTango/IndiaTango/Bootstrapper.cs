c43630b7ac3b891a17bb02ced9ccf24a3fa1f522
Luke Barnett <luke@barnett.net.nz>
2012-02-10 11:11:39 +1300

    Removed unused views to reduce clutter

--- IndiaTango/IndiaTango/Bootstrapper.cs
+++ IndiaTango/IndiaTango/Bootstrapper.cs
@@ -13,24 +13,15 @@ namespace IndiaTango
         {
             _container = new SimpleContainer();
 
-            _container.RegisterSingleton(typeof(MainViewModel), "MainViewModel", typeof(MainViewModel));
-            _container.RegisterPerRequest(typeof(GraphViewModel), "GraphViewModel", typeof(GraphViewModel));
-            _container.RegisterPerRequest(typeof(SessionViewModel), "SessionViewModel", typeof(SessionViewModel));
-            _container.RegisterPerRequest(typeof(BuoyDetailsViewModel), "BuoyDetailsViewModel", typeof(BuoyDetailsViewModel));
-            _container.RegisterPerRequest(typeof(MissingValuesViewModel), "MissingValuesViewModel", typeof(MissingValuesViewModel));
             _container.RegisterPerRequest(typeof(ContactEditorViewModel), "ContactEditorViewModel", typeof(ContactEditorViewModel));
             _container.RegisterPerRequest(typeof(EditSensorViewModel), "EditSensorViewModel", typeof(EditSensorViewModel));
             _container.RegisterPerRequest(typeof(SpecifyValueViewModel), "SpecifyValueViewModel", typeof(SpecifyValueViewModel));
             _container.RegisterPerRequest(typeof(SensorTemplateManagerViewModel), "SensorTemplateManagerViewModel", typeof(SensorTemplateManagerViewModel));
             _container.RegisterPerRequest(typeof(ExportViewModel), "ExportViewModel", typeof(ExportViewModel));
-            _container.RegisterPerRequest(typeof(OutlierDetectionViewModel), "OutlierDetectionViewModel", typeof(OutlierDetectionViewModel));
-            _container.RegisterPerRequest(typeof(CalibrateSensorsViewModel), "CalibrateSensorsViewModel", typeof(CalibrateSensorsViewModel));
             _container.RegisterPerRequest(typeof(SettingsViewModel), "SettingsViewModel", typeof(SettingsViewModel));
             _container.RegisterSingleton(typeof(LogWindowViewModel), "LogWindowViewModel", typeof(LogWindowViewModel));
             _container.RegisterPerRequest(typeof(ExportToImageViewModel), "ExportToImageViewModel", typeof(ExportToImageViewModel));
-            _container.RegisterPerRequest(typeof(ErroneousValuesDetectionViewModel), "ErroneousValuesDetectionViewModel", typeof(ErroneousValuesDetectionViewModel));
             _container.RegisterPerRequest(typeof(UseSelectedRangeViewModel), "UseSelectedRangeViewModel", typeof(UseSelectedRangeViewModel));
-            _container.RegisterPerRequest(typeof(WizardViewModel), "WizardViewModel", typeof(WizardViewModel));
             _container.RegisterSingleton(typeof(MainWindowViewModel), "MainWindowViewModel", typeof(MainWindowViewModel));
             _container.RegisterPerRequest(typeof(EditSiteDataViewModel), "EditSiteDataViewModel", typeof(EditSiteDataViewModel));
             _container.RegisterPerRequest(typeof(MatchToExistingSensorsViewModel), "MatchToExistingSensorsViewModel", typeof(MatchToExistingSensorsViewModel));
